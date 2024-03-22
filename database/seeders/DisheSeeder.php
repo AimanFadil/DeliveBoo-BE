6e8af1cc03f596033c741e67b7286daf6d0b6dec
AimanFadil <aimanbello2001@gmail.com>
2024-03-22 12:41:35 +0100

    Typology Seeder Done [ADD]

--- database/seeders/DisheSeeder.php
+++ database/seeders/DisheSeeder.php
@@ -18,6 +18,64 @@ class DisheSeeder extends Seeder
      */
     public function run()
     {
-        
+        $rests = [
+            [
+                'name' => 'Carbonara',
+                'ingredients' => 'Uova, Guanciale, pasta e pecorino',
+                'description' => 'Pasta alla Carbonara',
+                'price' => '13.00',
+                'visible' => true,
+                'image' => 'https://blog.giallozafferano.it/dulcisinforno/wp-content/uploads/2021/03/Carbonara-ricetta-5328.jpg',
+            ],
+            [
+                'name' => 'Pizza Quattro Stagioni',
+                'ingredients' => 'Carciofi, Funghi, Olive e Prosciutto Cotto',
+                'description' => 'È preparata tradizionalmente con pasta per pizza e una base con salsa di pomodori pelati, mozzarella e olio extravergine di oliva; nelle quattro diverse sezioni sono poi sistemati separatamente.',
+                'price' => '11.00',
+                'visible' => true,
+                'image' => 'https://www.petitchef.it/imgupl/recipe/pizza-quattro-stagioni--327908p533338.jpg',
+            ],
+            [
+                'name' => 'Uramaki Salmon',
+                'ingredients' => 'Alga Nori, Riso, Salmone, Salsa Terayaky, Philadelphia e Avocado',
+                'description' => 'Gli uramaki sono infatti rotolini di riso con all interno una foglia di alga nori arrotolata e il ripieno, mentre sulla parte esterna c è il riso, decorato con semi di sesamo nero o bianco tostati o uova di pesce volante, conosciuto nei ristoranti sushi con il nome di Tobiko.',
+                'price' => '8.00',
+                'visible' => true,
+                'image' => 'https://cdn.cook.stbm.it/thumbnails/ricette/145/145017/hd750x421.jpg',
+            ],
+            [
+                'name' => 'Gran Crispy MacBacon',
+                'ingredients' => 'Pancetta, Carne, Cheddar, Pane e Salsa Crispy',
+                'description' => 'Panino singolo non compreso Menu',
+                'price' => '11.00',
+                'visible' => true,
+                'image' => 'https://www.mcdonalds.it/sites/default/files/styles/compressed/public/products/gran-crispy-mcbacon--hero-mob_0.jpg?itok=IxGQroUp',
+            ],
+            [
+                'name' => 'Gyoza',
+                'ingredients' => 'Carne, Verdure, Sedano, Cipolle, Sfoglia di Pasta di Riso',
+                'description' => '',
+                'price' => '5.00',
+                'visible' => false,
+                'image' => 'https://media-assets.lacucinaitaliana.it/photos/61fd2d235455c3ec4f002f7c/1:1/w_2560%2Cc_limit/Gyoza-alla-piastra.jpg',
+            ],
+            
+        ];
+
+        foreach ($rests as $rest) {
+            $new_dishe = new Dishe();
+            $new_dishe->name = $rest['name'];
+            $new_dishe->ingredients = $rest['ingredients'];
+            $new_dishe->description = $rest['description'];
+            $new_dishe->price = $rest['price'];
+            $new_dishe->visible = $rest['visible'];
+            $new_dishe->image = $rest['image'];
+            $new_dishe->slug = Str::slug($new_dishe->name, '-');
+            $new_dishe->save();
+            $new_dishe->slug = Str::slug($new_dishe->name, '-').'-'.$new_dishe->id;
+            $new_dishe->save();
+
+
+        }
     }
 }
--- database/seeders/TypologySeeder.php
+++ database/seeders/TypologySeeder.php
@@ -17,6 +17,68 @@ class TypologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $rests = [
+            [
+                'name' => 'Panini',
+                'type_image' => 'https://www.sandanielemagazine.com/wp-content/uploads/2021/01/panino-gourmet-con-carciofi-fritti-e-Prosciutto-di-San-Daniele.jpg',
+            ],
+            [
+                'name' => 'Kebab',
+                'type_image' => 'https://media.istockphoto.com/id/851493796/it/foto/primo-passo-di-kebab-sandwich.jpg?s=612x612&w=0&k=20&c=A7Kj4tD9T7UEQnzwB72PX2i2IcgUqLw62A0swrmUlVc=',
+            ],
+            [
+                'name' => 'Sushi',
+                'type_image' => 'https://img.freepik.com/premium-photo/sushi-withe-background-ai-generative_514344-4128.jpg',
+            ],
+            [
+                'name' => 'Cinese',
+                'type_image' => 'https://media.istockphoto.com/id/1292747109/photo/fried-wonton-plates.jpg?s=612x612&w=0&k=20&c=r5DmYgtYR3ks3xISIDjYdEMZ89kv9cHH0kz9LI2S_3o=',
+            ],
+            [
+                'name' => 'Pizza',
+                'type_image' => 'https://img.freepik.com/premium-photo/pizza-margherita-with-tomatoes-basil-mozzarella-cheese-isolated-white-background_87414-5001.jpg',
+            ],
+            [
+                'name' => 'Piadine',
+                'type_image' => 'https://t4.ftcdn.net/jpg/00/36/10/19/360_F_36101927_PiATzeSryReMtziiFNIRekmHZk79Xiln.jpg',
+            ],
+            [
+                'name' => 'Indiano',
+                'type_image' => 'https://img.freepik.com/premium-photo/bowl-chicken-curry-with-rice-white-background_787273-4129.jpg',
+            ],
+            [
+                'name' => 'Messicano',
+                'type_image' => 'https://img.freepik.com/premium-photo/two-tacos-with-white-background_871710-1629.jpg',
+            ],
+            [
+                'name' => 'FastFood',
+                'type_image' => 'https://img.freepik.com/premium-photo/burger-white-background_398492-13237.jpg',
+            ],
+            [
+                'name' => 'Ristorante',
+                'type_image' => 'https://img.freepik.com/premium-photo/restaurant-food-white-background-restaurant-menu-photos-menu_830198-529.jpg',
+            ],
+            [
+                'name' => 'SteakHouse',
+                'type_image' => 'https://img.freepik.com/premium-photo/grilled-porterhouse-steak-chopping-board-cooked-beef-meat-isolated-white-background_89816-45857.jpg',
+            ],
+            [
+                'name' => 'Pesce',
+                'type_image' => 'https://img.freepik.com/premium-photo/cooked-fish-plate-white-background_252187-3120.jpg',
+            ],
+            [
+                'name' => 'Dessert',
+                'type_image' => 'https://img.freepik.com/premium-photo/sweet-tiramisu-isolated-white-background-tasty-dessert_185193-4072.jpg',
+            ],
+            
+        ];
+
+        foreach ($rests as $rest) {
+            $new_type = new Typology();
+            $new_type->name = $rest['name'];
+            $new_type->type_image = $rest['type_image'];
+            $new_type->save();
+
+        }
     }
 }
