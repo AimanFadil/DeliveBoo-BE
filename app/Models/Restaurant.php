b839440805ab0719e36220d9dc3b0b17075851ad
AimanFadil <aimanbello2001@gmail.com>
2024-03-22 16:01:09 +0100

    Migration Relations [ADD]

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+    public function typologies()
+    {
+        return $this->belongsToMany(Typology::class);
+    }
+    public function dishes()
+    {
+        return $this->hasMany(Dishe::class);
+    }
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+
 }
