265a80c62daf519abdc89373a9d607fa225ea3f9
SebastianBuftea <sebibuftea85@gmail.com>
2024-03-23 14:21:18 +0100

    [ADD] Validation of Dishes and Store Order

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -13,7 +13,7 @@ class StoreOrderRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,25 @@ class StoreOrderRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|max:50',
+            'mail' => 'required|max:70',
+            'address' => 'required|max:200',
+            'date_delivery' => 'required',
+            'phone' => 'min:10|max:14'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => 'Inserire il nome',
+            'name.max' => 'La lunghezza massima consentita per il nome è di 50 caratteri',
+            'mail.required' => 'Inserie unindirizzo mail',
+            'mail.max' => 'La lunghezza massima consentita per la mail è di 70 caratteri',
+            'address.required' => 'Inserire un indirizzo per la consegna',
+            'address.max' => "La lunghezza massima consentita per l'indirizzo è di 70 caratteri",
+            'date_delivery.required' => 'Inserire la data di consegna',
+            'phone.min' => 'Inserire un Numero di telefono valido, i numeri inseriti sono meno di 10',
+            'phone.max' => 'Inserire un Numero di telefono valido, i numeri inseriti sono più di 14',
         ];
     }
 }
