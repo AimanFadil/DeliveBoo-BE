2549abd80c57f03633a91cddf0adeeb50ca51425
stappost <emanuele.scarpantoni@gmail.com>
2024-03-25 15:54:30 +0100

    fix button creation restaurant

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,13 @@
                                 </li>
                             @endif
                         @else
+                            @if (!Auth::user()->restaurant()->exists('user_id'))
+                                <li class="nav-item">
+                                    <a class="btn btn-sm btn-primary mt-1 me-3"
+                                        href="{{ url('restaurants/create') }}">{{ __('Create your Restaurant') }}
+                                    </a>
+                                </li>
+                            @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle colorgreen red-hover" href="#"
                                     role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
@@ -80,8 +87,6 @@
                                         href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
                                     <a class="dropdown-item colorgreen red-hover"
                                         href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                                    <a class="dropdown-item colorgreen red-hover"
-                                        href="{{ url('restaurants/create') }}">{{ __('Create your Restaurant') }}</a>
                                     <a class="dropdown-item colorgreen red-hover" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
