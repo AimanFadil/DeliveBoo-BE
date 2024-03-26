b83d028356d1b999a9d636834ae6f0794f18e4c3
stappost <emanuele.scarpantoni@gmail.com>
2024-03-26 13:33:43 +0100

    add check for restaurant and dishes edit

--- resources/views/admin/dish/edit.blade.php
+++ resources/views/admin/dish/edit.blade.php
@@ -1,86 +1,97 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container ">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="fs-4  my-4 colorgreen fw-bold">
-                    Modifica il tuo Piatto
-                </h1>
-            </div>
-            <div class="col-12 ">
-                <form action="{{ route('admin.dish.update', ['dish' => $dish->id]) }}" method="POST"
-                    class="form-control my-4 border-success-subtle border-2 bg-forms colorgreen container-shadow"
-                    enctype="multipart/form-data">
-                    @csrf
-                    @method('PUT')
+    @if (Auth::user()->restaurant()->exists('user_id') && $dish->restaurant_id === Auth::user()->restaurant->id)
+        <div class="container ">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="fs-4  my-4 colorgreen fw-bold">
+                        Modifica il tuo Piatto
+                    </h1>
+                </div>
+                <div class="col-12 ">
+                    <form action="{{ route('admin.dish.update', ['dish' => $dish->id]) }}" method="POST"
+                        class="form-control my-4 border-success-subtle border-2 bg-forms colorgreen container-shadow"
+                        enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
 
 
-                    <label for="name" class="form-label fw-semibold">Nome Piatto</label>
-                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                        name="name" value="{{ old('name') }}">
+                        <label for="name" class="form-label fw-semibold">Nome Piatto</label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                            name="name" value="{{ old('name') }}">
 
-                    @error('name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                    <hr>
+                        <hr>
 
-                    <label for="ingredients" class="form-label fw-semibold">Ingredienti</label>
-                    <input type="text" class="form-control @error('ingredients') is-invalid @enderror" id="ingredients"
-                        name="ingredients" value="{{ old('ingredients') }}">
+                        <label for="ingredients" class="form-label fw-semibold">Ingredienti</label>
+                        <input type="text" class="form-control @error('ingredients') is-invalid @enderror"
+                            id="ingredients" name="ingredients" value="{{ old('ingredients') }}">
 
-                    @error('ingredients')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                        @error('ingredients')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                    <hr>
+                        <hr>
 
-                    <label for="description" class="form-label fw-semibold">Descrizione</label>
-                    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
-                        rows="3">{{ old('description')}}</textarea>
-
-
-                    @error('description')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-
-                    <hr>
-
-                    <label for="price" class="form-label fw-semibold">Prezzo</label>
-                    <input type="number" class="form-control @error('price') is-invalid @enderror" id="price"
-                        name="price" value="{{ old('price') }}">
+                        <label for="description" class="form-label fw-semibold">Descrizione</label>
+                        <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                            rows="3">{{ old('description') }}</textarea>
 
 
-                    @error('price')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                        @error('description')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                    <label for="image" class="form-label">Immagine</label>
-                    <input type="file" class="form-control" id="image" name="image">
+                        <hr>
 
-                    <label for="visible" class="form-label">Disponibile</label>
-                    <select id="visible" name="visible" class="form-select" value="{{ $dish->visible }}">
-                        <option value="1">si</option>
-                        <option value="0">no</option>
-                    </select>
-
-                    <div class="col-12 d-flex justify-content-end">
-                        <button type="submit" class="btn btn-sm btn-success fw-semibold m-4 p-2 hover-3">Modifica</button>
-                    </div>
+                        <label for="price" class="form-label fw-semibold">Prezzo</label>
+                        <input type="number" class="form-control @error('price') is-invalid @enderror" id="price"
+                            name="price" value="{{ old('price') }}">
 
 
+                        @error('price')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                </form>
+                        <label for="image" class="form-label">Immagine</label>
+                        <input type="file" class="form-control" id="image" name="image">
+
+                        <label for="visible" class="form-label">Disponibile</label>
+                        <select id="visible" name="visible" class="form-select" value="{{ $dish->visible }}">
+                            <option value="1">si</option>
+                            <option value="0">no</option>
+                        </select>
+
+                        <div class="col-12 d-flex justify-content-end">
+                            <button type="submit"
+                                class="btn btn-sm btn-success fw-semibold m-4 p-2 hover-3">Modifica</button>
+                        </div>
+
+
+
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
+    @else
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Piatto non disponibile</h2>
+                </div>
+            </div>
+        </div>
+    @endif
 @endSection
