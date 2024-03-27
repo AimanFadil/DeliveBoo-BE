3f9804c8d6c563b0c9e4fec7a238626d7cc987eb
AimanFadil <aimanbello2001@gmail.com>
2024-03-27 11:12:46 +0100

    Api Controller [ADD]

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\Api\RestaurantController as RestaurantController;
+
+use App\Http\Controllers\Api\TypologyController as TypologyController;
+
+use App\Http\Controllers\Api\DisheController as DisheController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/restaurant', [RestaurantController::class, 'index']);
+Route::get('/typology', [TypologyController::class, 'index']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
