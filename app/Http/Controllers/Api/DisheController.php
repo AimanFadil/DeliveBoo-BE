6f03a1a9d014debab2b054e873ab3a5fb8897e3c
AimanFadil <aimanbello2001@gmail.com>
2024-03-29 11:21:53 +0100

    dishe api

--- app/Http/Controllers/Api/DisheController.php
+++ app/Http/Controllers/Api/DisheController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Dishe;
 
 class DisheController extends Controller
 {
-    //
+    public function index()
+    {
+        $dishes = Dishe::all();
+        
+        return response()->json([
+            "success" => true,
+            "results" => $dishes
+        ]);
+    }
 }
