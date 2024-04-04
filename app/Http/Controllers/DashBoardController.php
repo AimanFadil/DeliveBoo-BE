2e83f879a44f667c25eba2354b542428f07265c1
stappost <emanuele.scarpantoni@gmail.com>
2024-04-04 09:29:33 +0200

    add table orders in dashboard

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Restaurant;
 use App\Models\User;
+use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -13,12 +14,13 @@ class DashBoardController extends Controller
     public function index()
     {
         $restaurant =  Restaurant::where('user_id', '=', Auth::id())->first();
+        $orders = Order::where('restaurant_id', $restaurant->id)->get();
         $typologies = [];
         if ($restaurant != null ) {
 
             $typologies = $restaurant->typologies()->get();
             
         }
-        return view('dashboard', compact('restaurant', 'typologies'));
+        return view('dashboard', compact('restaurant', 'typologies','orders'));
     }
 }
